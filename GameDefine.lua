b8967c0ef507cae18e7d5904d7ff6f807ed1a780
zhiqizhang <zhiqizhang@moonton.com>
2016-07-25 17:32:03 +0800

    调整目录结构

---
+++ GameDefine.lua
@@ -0,0 +1,47 @@
+
+MoveDirectionType = {
+    idle  = 1,
+	left  = 2,
+    right = 3,
+    up    = 4,
+    down  = 5,
+}
+
+--TiledMap 
+TiledMapLayer = {
+	barrier = "Barrier",
+	background = "BackGround",
+}
+
+--动作类型
+AnimationType = {
+	idleD = 1,
+	idleL = 2,
+	idleR = 3,
+	idleU = 4,
+	walkD = 5,
+	walkL = 6, 
+	walkR = 7,
+	walkU = 8,
+}
+
+--战斗阶段
+BattleStage = {
+	init   = 0,
+	level1 = 1,
+	level2 = 2,
+	level3 = 3,
+	ended = 4,
+}
+
+--玩家类型
+PlayerType = {
+	player = 1,
+	enemy  = 2,
+}
+
+SkillType = {
+	static = 1,
+	disposable  = 2,
+}
+
